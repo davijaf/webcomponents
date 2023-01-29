92c960f42693c536f06c6370343197681861e906
davijaf <davijose@gmail.com>
2023-01-29 00:42:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Portal de Noticias Galáctica
+Este é o repositório do Portal de Noticias Galáctica, uma aplicação web desenvolvida utilizando web components.
+
+## Recursos Utilizados
+* HTML
+* CSS (styles/cards.css)
+* JavaScript (src/Components/*.js)
+
+## Componentes
+* `<hello-world>`: Exibe uma mensagem de boas-vindas.
+* `<titulo-dinamico>`: Exibe um título dinâmico, que pode ser definido através da propriedade titulo.
+* `<card-news>`: Exibe uma notícia, com as propriedades: autor, title, link-url, link-image, content.
+
+Este código define um componente web personalizado chamado "card-news" usando a classe JavaScript "CardNews" que estende a classe built-in "HTMLElement".
+
+A classe possui uma função construtora que anexa um shadow DOM ao componente, e adiciona os métodos "build" e "styles" a ele.
+
+O método "build" cria a estrutura do componente, incluindo divs para as seções esquerda e direita do cartão, elementos como spans, anchors e images, e define suas propriedades e conteúdo com base nas atributos passados para o componente quando ele é usado na página HTML.
+
+O método "styles" cria um elemento "style" e define os estilos CSS para o componente.
+
+Por fim, o elemento personalizado é registrado usando o método "customElements.define".
+
+## Execução
+
+Para executar a aplicação, basta abrir o arquivo index.html em um navegador.
---
+++ src/Components/CardNews.js
@@ -0,0 +1,101 @@
+class CardNews extends HTMLElement {
+    constructor(){
+        super();
+
+        // base do component
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build(){
+        // enviar para a shadow
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const card_left = document.createElement("div");
+        card_left.setAttribute("class", "card_left");
+        componentRoot.appendChild(card_left);
+
+        const autor = document.createElement("span");
+        autor.textContent = "By "+ (this.getAttribute("autor") || "Anonymous");
+        card_left.appendChild(autor);
+
+
+        const linkTitle = document.createElement("a");
+        linkTitle.setAttribute("href", "#");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = (this.getAttribute("link-url") || "#");
+        card_left.appendChild(linkTitle);
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+        card_left.appendChild(newsContent);
+
+        const card_right = document.createElement("div");
+        card_right.setAttribute("class", "card_right");
+        componentRoot.appendChild(card_right);
+
+        const imageNews = document.createElement("img");
+        imageNews.src = this.getAttribute("link-image") || "assets/anonymous.jpg";
+        card_right.appendChild(imageNews);
+
+        return componentRoot;
+    }
+
+    styles(){
+        //estilizar o component
+        const style = document.createElement("style");
+
+        style.textContent = `
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Segoe UI', sans-serif;
+
+        }
+
+        .card{
+            width: 60%;
+            height: 400x;
+            border: 1px solid gray;
+            display: flex;
+            flex-direction: row;
+            box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+        -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+        -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+            justify-content: space-between;
+        }
+
+        .card_left{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        .card_left > a {
+            margin-top: 15px;
+            font-size: 30px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+        .card_left >  p {
+            color: rgb(70,70,70);
+        }
+
+        .card_left >  span {
+            font-weight: 400;
+        }
+
+        .card_right > img {
+            max-width: 300px;
+        }
+        `
+        return style;
+    }
+}
+
+customElements.define('card-news', CardNews);
